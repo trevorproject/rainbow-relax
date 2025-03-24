12fb571ea432f8ecd5372001b1ba0fd1ad6e00ed
Sebastian De los Santos <sebastiandelossantos@MacBook-Pro-de-Sebastian.local>
2025-03-24 16:46:37 -0600

    new size toggle button

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -5,37 +5,37 @@ import usaFlag from "../assets/usa-flag.png";
 
 const ToggleButton = () => {
   const { i18n } = useTranslation();
-  const [isOn, setIsOn] = useState(i18n.language === "es" ? false : true);
+  const [isOn, setIsOn] = useState(i18n.language === "es" ? true : false);
 
   const handleChangeLanguage = () => {
     setIsOn(!isOn);
-    i18n.changeLanguage(i18n.language === "en" ? "es" : "en");
+    i18n.changeLanguage(i18n.language === "es" ? "en" : "es");
   };
 
   return (
     <button
       onClick={handleChangeLanguage}
-      className="w-10 h-6 sm:w-14 sm:h-8 rounded-full relative bg-[var(--color-button)]"
+      className="w-16 h-8 sm:w-16 sm:h-8 rounded-full relative bg-[var(--color-button)]"
     >
       <div
-        className={`w-4 h-4 sm:w-6 sm:h-6 rounded-full absolute top-1 transition-transform bg-cover bg-center flex justify-center items-center ${
+        className={`w-6 h-6 sm:w-6 sm:h-6 rounded-full absolute top-1 transition-transform bg-cover bg-center flex justify-end items-center ${
           isOn
-            ? "translate-x-1 sm:translate-x-1"
-            : "translate-x-5 sm:translate-x-7"
+            ? "translate-x-0 sm:translate-x-0"
+            : "translate-x-7 sm:translate-x-7"
         }`}
       >
         <p className="text-[--font-global] text-white text-[12px]">
-          {isOn ? "En" : "Es"}
+          {isOn ? "Es" : "En"}
         </p>
       </div>
       <div
-        className={`w-4 h-4 sm:w-6 sm:h-6 rounded-full absolute top-1 transition-transform bg-cover bg-center ${
+        className={`w-6 h-6 sm:w-6 sm:h-6 rounded-full absolute top-1 transition-transform bg-cover bg-center ${
           isOn
-            ? "translate-x-5 sm:translate-x-7"
+            ? "translate-x-9 sm:translate-x-9"
             : "translate-x-1 sm:translate-x-1"
         }`}
         style={{
-          backgroundImage: !isOn ? `url(${mexicoFlag})` : `url(${usaFlag})`,
+          backgroundImage: isOn ? `url(${mexicoFlag})` : `url(${usaFlag})`,
         }}
       ></div>
     </button>
