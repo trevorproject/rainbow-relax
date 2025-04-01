42e1f32b0064e3d4012cf4379e28422172159a40
MiguelCarreraE <90919223+MiguelCarreraE@users.noreply.github.com>
2025-03-31 20:57:28 -0600

    MPI-42: Main Animation (#12)
    * Main Animation
    
    * Modified App.tsx
    
    ---------
    
    Co-authored-by: MiguelCarreraE <miguelcarrera2003@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@ import NavBar from "./components/NavBar";
 import WelcomePage from "./components/WelcomePage";
 import { init } from "./i18n/init";
 import QuickEscape from "./components/QuickEscape";
+import { MainAnimation } from "./components/MainAnimation";
+import { MainAnimationProvider } from "./context/MainAnimationProvider";
+
 
 init();
 function App() {
   return (
-    <>
+    <MainAnimationProvider>
       <div>
-        <NavBar />
+        <div>
+          <NavBar />
+        </div>
+        <div className="flex flex-col items-center justify-center min-h-screen text-[var(--color-text)]">
+          <QuickEscape />
+          <WelcomePage />
+          <MainAnimation />
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-[var(--background-global)] text-[var(--color-text)]">
-        <QuickEscape />
-        <WelcomePage />
-      </div>
-    </>
+    </MainAnimationProvider>
   );
 }
 
