2b71269d64e343c3b094e9f543f2da0849c727b8
Laura Reyes <laurarey1019@gmail.com>
2025-05-05 22:52:18 -0600

    Add some details

--- src/components/ThankYouPage.tsx
+++ src/components/ThankYouPage.tsx
@@ -2,14 +2,14 @@ import { useTranslation } from "react-i18next";
 
 const ThankYouPage = () => {
   const { t } = useTranslation();
-  const linkClass = "font-bold text-[#4E4E4E] mb-8 px-6 py-3 underline hover:opacity-80 transition";
+  const linkClass = "font-bold text-[#4E4E4E] text-base sm:text-lg md:text-xl lg:text-2xl mb-8 px-6 py-3 underline hover:opacity-80 transition";
 
   return (
-    <div className="mt-10 flex flex-col items-center justify-center w-full gap-y-8 px-4">
-      <h1 className="font-bold text-[#4E4E4E] text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px] text-center ">
+    <div className="mt-10 flex flex-col items-center justify-center w-full gap-y-6 px-4">
+      <h1 className="font-bold text-[#4E4E4E] text-center text-[clamp(2rem,5vw,3.125rem)] max-w-[90%] sm:max-w-[75%] md:max-w-[50%] mx-auto">
         {t("repeat-instruction")}
       </h1>      
-      <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4 text-[var(--color-primary)]">
+      <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-4">
         <a href="https://www.thetrevorproject.org/breathing-exercise/"
         className={linkClass}
         >
