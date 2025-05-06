3d7db6d7d17f79d3577ff88e185fb47c88648fe3
Laura Reyes <laurarey1019@gmail.com>
2025-05-05 18:58:08 -0600

    First test

---
+++ src/components/ThankYouPage.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+
+const ThankYouPage = () => {
+  const { t } = useTranslation();
+  const linkClass =
+    "font-bold text-[#4E4E4E] mb-8 px-6 py-3 underline hover:opacity-80 transition";
+
+  return (
+    <div className="mt-10 flex flex-col items-center justify-center w-full gap-y-8">
+      <h1 className="font-bold text-[#4E4E4E] text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px] text-[--font-global] text-center md:text-left">
+        {t("repeat-instruction")}
+      </h1>      
+      <div className="flex flex-col gap-2 text-[var(--color-primary)]">
+        <a href="https://www.thetrevorproject.org/breathing-exercise/"
+        className={linkClass}
+        >
+        {t("try-again-label")}
+        </a>
+        <a
+          href="https://www.thetrevorproject.mx/ayuda/"
+          className={linkClass}
+        >
+          {t("get-help-label")}
+        </a>
+      </div>
+      <a
+          href="https://www.thetrevorproject.mx/dona/"
+          className={linkClass}
+    >
+          {t("Donate")}
+        </a>
+      
+    </div>
+  );
+};
+
+export default ThankYouPage;
--- src/router/routesEnum.ts
+++ src/router/routesEnum.ts
@@ -1,4 +1,5 @@
 export enum RoutesEnum {
     HOME = "/",
     BREATHING = "/breathing",
+    THANKYOU = "/thankyou",
   }
