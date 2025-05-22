e296babb5483384395cbda76734edfa3289da8ec
Laura Reyes <laurarey1019@gmail.com>
2025-05-22 10:44:40 -0600

    change message important

--- src/components/ThankYouPage.tsx
+++ src/components/ThankYouPage.tsx
@@ -12,14 +12,14 @@ const ThankYouPage = () => {
   return (
     <div className="mt-10 flex flex-col items-center justify-center w-full gap-y-6 px-4">
       <h1 className="font-bold text-[#4E4E4E] text-center text-[clamp(2rem,5vw,3.125rem)] max-w-[90%] sm:max-w-[75%] md:max-w-[50%] mx-auto">
+        {message && (
+          <p>{message}
+          </p>
+        )}
+      </h1>
+      <p className="text-center font-bold text-xl text-[#4E4E4E] max-w-[600px]">
         {t("repeat-instruction")}
-      </h1> 
-       {/* Affirmation message */}
-       {message && (
-        <p className="text-center font-bold text-xl text-[#4E4E4E] max-w-[600px]">
-          {message}
-        </p>
-      )}
+      </p> 
       <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-2 sm:gap-4">
         <a href="/"
         className={linkClass}
