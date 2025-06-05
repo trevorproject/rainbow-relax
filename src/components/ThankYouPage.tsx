d99fb323a2d1d2c4c0d03bc4de87efb17d5d4215
Laura Reyes <112602724+Recaclo@users.noreply.github.com>
2025-06-05 09:06:05 -0600

    Fix/MPI-72  (#26) Url fix

--- src/components/ThankYouPage.tsx
+++ src/components/ThankYouPage.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { useAffirmationMessage } from "../hooks/useAffirmationMessages";
+import { NavLink } from "react-router";
 
 const ThankYouPage = () => {
   const { t, i18n } = useTranslation();
@@ -12,33 +13,31 @@ const ThankYouPage = () => {
   return (
     <div className="mt-10 flex flex-col items-center justify-center w-full gap-y-6 px-4">
       <h1 className="font-bold text-[#4E4E4E] text-center text-[clamp(2rem,5vw,3.125rem)] max-w-[90%] sm:max-w-[75%] md:max-w-[50%] mx-auto">
+        {message && (
+          <p>{message}
+          </p>
+        )}
+      </h1>
+      <p className="text-center font-bold text-xl text-[#4E4E4E] max-w-[600px]">
         {t("repeat-instruction")}
-      </h1> 
-       {/* Affirmation message */}
-       {message && (
-        <p className="text-center font-bold text-xl text-[#4E4E4E] max-w-[600px]">
-          {message}
-        </p>
-      )}
+      </p> 
       <div className="flex flex-col sm:flex-row flex-wrap justify-center gap-2 sm:gap-4">
-        <a href="/"
-        className={linkClass}
-        >
+        <NavLink to="/" className={linkClass}>
         {t("try-again-label")}
-        </a>
-        <a
-          href={getHelpUrl}
+        </NavLink>
+        <NavLink
+          to={getHelpUrl}
           className={linkClass}
         >
           {t("get-help-label")}
-        </a>
+        </NavLink>
       </div>
-      <a
-          href= {donateUrl}
+      <NavLink
+          to= {donateUrl}
           className={linkClass}
     >
           {t("Donate")}
-        </a>
+        </NavLink>
       
     </div>
   );
