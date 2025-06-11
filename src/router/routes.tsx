1aaecee0f7f274f47bf76d56ef2f3dc279e11baf
Sebastian De los Santos <sdlsh31@gmail.com>
2025-06-11 15:35:45 -0400

    parameter-quick-escape

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,21 +4,34 @@ import QuickEscape from "../components/QuickEscape";
 import { RoutesEnum } from "./routesEnum";
 import BreathingInstructions from "../components/Instructions";
 import ThankYouPage from "../components/ThankYouPage";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export function AppRoutes() {
+  const [showQuickEscape, setShowQuickEscape] = useState(false);
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data?.showQuickEscape !== undefined) {
+        setShowQuickEscape(event.data.showQuickEscape);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
   return (
     <Routes>
       <Route
         path={RoutesEnum.HOME}
         element={
           <>
-            <QuickEscape />
+            <QuickEscape showQuickEscape={showQuickEscape} />
             <WelcomePage />
           </>
         }
       />
-      <Route path={RoutesEnum.BREATHING} element={<BreathingInstructions/>} />
-      <Route path={RoutesEnum.THANKYOU}  element={<ThankYouPage />} />
+      <Route path={RoutesEnum.BREATHING} element={<BreathingInstructions />} />
+      <Route path={RoutesEnum.THANKYOU} element={<ThankYouPage />} />
     </Routes>
   );
 }
