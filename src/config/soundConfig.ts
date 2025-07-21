fcf05856d69337a28ab8793f60056de1ed559937
Sebastian De los Santos <sdlsh31@gmail.com>
2025-07-21 12:13:20 -0600

    sound-effects-feature

--- src/config/soundConfig.ts
+++ src/config/soundConfig.ts
@@ -1,5 +1,7 @@
-import { HowlOptions } from 'howler';
-import backgroundSound from '../assets/sounds/Background.mp3';
+import { HowlOptions } from "howler";
+import backgroundSound from "../assets/sounds/Background.mp3";
+import cycleInstructionsEs from "../assets/sounds/cycle-es.mp3";
+import cycleInstructionsEn from "../assets/sounds/cycle-en.mp3";
 
 export const soundConfig: Record<string, HowlOptions> = {
   "4-7-8": {
@@ -9,5 +11,14 @@ export const soundConfig: Record<string, HowlOptions> = {
   },
 };
 
+export const getInstructionsConfig = (
+  lang: string
+): Record<string, HowlOptions> => ({
+  "4-7-8": {
+    src: lang === "es" ? [cycleInstructionsEs] : [cycleInstructionsEn],
+    loop: true,
+    volume: 0.4,
+  },
+});
+
 export type SoundKey = keyof typeof soundConfig | string;
- 
