f4da573862baac94b0b94e2d5d56feb669290e4c
GuillermoC66 <72630022+GuillermoC66@users.noreply.github.com>
2025-09-05 18:33:19 +0000

    Made env variable

--- src/components/GA4.tsx
+++ src/components/GA4.tsx
@@ -2,39 +2,41 @@ import ReactGA from "react-ga4";
 import CookieConsent from "react-cookie-consent";
 
 const GA4 = () => {
-  var gtag = "your gtag tag"; // for us rn G-7ZZ5Z0WSKJ	
+  var gtag = import.meta.env.VITE_GTAG;	
+  if (gtag==null)
+  {
+    return null
+  }else
+  {
+    return (
+      <CookieConsent
+        location="bottom"
+        buttonText="I Accept"
+        cookieName="cookie1"
+        style={{ background: "#ff5a3e" }}
+        buttonStyle={{ color: "#595c3fff", fontSize: "13px" }}
+        expires={150}
+         onAccept={(acceptedByScrolling) => {
+         if (acceptedByScrolling) {
+           // triggered if user scrolls past threshold, dna for our case but this didnt work if i didnt write it
+         } else {
+           //initialize GA4 and anonimize IP
+           ReactGA.initialize(gtag, {
+           gaOptions: {
+           anonymizeIp: true,
+           },
+         })
+         }
+        }}
+        enableDeclineButton flipButtons
+          onDecline={() => {
+        }}
 
-  return (
-    <CookieConsent
-      location="bottom"
-      buttonText="Accept"
-      cookieName="galleta"
-      style={{ background: "#e34c00ff" }}
-      buttonStyle={{ color: "#595c3fff", fontSize: "13px" }}
-      expires={150}
-        onAccept={(acceptedByScrolling) => {
-        if (acceptedByScrolling) {
-          // triggered if user scrolls past threshold, dna for our case but this didnt work if i didnt write it
-          alert("Accept was triggered by user scrolling");
-        } else {
-          alert("Accept was triggered by clicking the Accept button");
-          //initialize GA4 and anonimize IP
-          ReactGA.initialize(gtag, {
-          gaOptions: {
-          anonymizeIp: true,
-          },
-        })
-        }
-      }}
-      enableDeclineButton flipButtons
-        onDecline={() => {
-          alert("ok so nothing happens we dont track");
-      }}
-        
-    >
-      This website uses google analytics to better user expericence{"\n"}Este sitio utiliza google analytics para mejorar la experiencia de usuario.{" "}
+      >
+        This website uses google analytics to better user expericence{"\n"}Este sitio utiliza google analytics para mejorar la experiencia de usuario.{" "}
 
 
-    </CookieConsent>)
+      </CookieConsent>)
+    }
   }
 export default GA4;
