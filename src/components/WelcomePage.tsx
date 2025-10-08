669587d8223d7c5daf6f5afdc977eae54d09897a
GuillermoC66 <72630022+GuillermoC66@users.noreply.github.com>
2025-10-08 17:15:30 +0000

    Button does whats expected now, but it changes the layout a bit, but I think it's acceptable.

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,30 +1,40 @@
 import { useTranslation } from "react-i18next";
 import QuickStartPreset from "./QuickStartPreset";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MainAnimationContext } from "../context/MainAnimationContext";
 
 const WelcomePage = () => {
   const { t } = useTranslation();
   const animation = useContext(MainAnimationContext);
+  const [isInfoVisible, setIsInfoVisible] = useState(false);
+
   useEffect(() => {
     animation.changeAnimation("main");
   }, []);
+
+    const toggleInfo = () => {
+    setIsInfoVisible(!isInfoVisible);
+  };
 
   return (
     <div className="flex flex-wrap justify-center items-start max-w-[70rem] max-h-[50vh] gap-6 px-4 md:gap-[2rem] md:px-8">
       <div className="flex max-w-[20rem] items-start">
         <h2 className="font-bold text-[#4E4E4E] text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px] text-[--font-global] text-center md:text-left">
           {t("title-text")}
-        {/*Explanation*/}
-      <span className="cursor-pointer text-center"></span>
-      <button id='infoButton' title={t("Explanation478")}
-      
-        className="hover:underline flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[var(--color-button)] bg-[var(--color-button-text)] rounded-full shadow-md hover:opacity-80 text-sm sm:text-base max-w-[5.5rem] items-center justify-center"
-      >
-        <p className="text-[--font-global] text-[15px] font-bold">i</p>
-        
-      </button>
-      <p id='infoText' className= "visibility: hidden flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[--font-global] text-[15px] text-[#4E4E4E] bg-[var(--color-button-text)] rounded-md">{t("Explanation478")}</p>
+          {/*Explanation*/}
+          <span title={t("Explanation478")} className="cursor-pointer text-center"></span>
+          <button 
+            id='infoButton' 
+            title={t("Explanation478")}
+            onClick={toggleInfo}
+            className="hover:underline flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[var(--color-button)] bg-[var(--color-button-text)] rounded-full shadow-md hover:opacity-80 text-sm sm:text-base max-w-[5.5rem] items-center justify-center"
+          >
+            <p className="text-[--font-global] text-[15px] font-bold">i</p>
+          </button>
+          <p id='infoText' 
+            className={`flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[--font-global] text-[15px] text-[#4E4E4E] bg-[var(--color-button-text)] rounded-md ${
+              isInfoVisible ? 'visible' : 'hidden'}`}
+          >{t("Explanation478")}</p>
         </h2>
       </div>
 
