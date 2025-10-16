03647b57619bfa043ee530a56d3b140e3eaef65b
GuillermoC66 <72630022+GuillermoC66@users.noreply.github.com>
2025-10-16 04:30:59 +0000

    Made animation folder and changed some colours

--- src/components/Animation/HomeAnimation.tsx
+++ src/components/Animation/HomeAnimation.tsx
@@ -33,14 +33,14 @@ const Band = ({ delay }: { delay: number }) => {
     }}
       initial={{
         opacity: 0,
-        scale: 3,
+        scale: 4,
         x: "-50%",
         y: "-50%",
         
       }}
       animate={{
         opacity: [0, 0.3, 1, 1, 0.3, 0],
-        scale: 1.5,
+        scale: 2,
       }}
       transition={{
         repeat: Infinity,
@@ -51,10 +51,10 @@ const Band = ({ delay }: { delay: number }) => {
         ease: "easeInOut",
         delay,
       }}
-      className=" opacity-50 fixed top-1/2 left-1/2
-      h-[20vw] w-[20vw] rounded-full  
-      bg-gradient-to-br from-[var(--color-button)] to-[var(--circle-bottom)]-500/20 
-      shadow-m [var(--color-button)]-500/20 overflow-auto"
+      className=" opacity-60 fixed top-1/2 left-1/2
+      h-[25vw] w-[25vw] rounded-full  
+      bg-gradient-to-br from-[#dac2fe] to-[#dac2f9]-500/20 
+      shadow-m [#ffcec5]-500/20 overflow-auto"
     />
   );
 };
