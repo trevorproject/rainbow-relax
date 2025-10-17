0cd010980b2953421647d61b0bc6268eb216c7f8
GuillermoC66 <72630022+GuillermoC66@users.noreply.github.com>
2025-10-17 04:14:38 +0000

    Made more animations

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -19,7 +19,7 @@ const WelcomePage = () => {
   return (
     <div className="flex flex-wrap justify-center items-start max-w-[70rem] max-h-[50vh] gap-6 px-4 md:gap-[2rem] md:px-8">
       <div className="flex max-w-[20rem] items-start">
-        <h2 className="font-bold text-[--color-text] text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px] text-[--font-global] text-center md:text-left z-2">
+        <h2 className="font-bold text-[--color-text]  text-[30px] sm:text-[35px] md:text-[40px] lg:text-[50px] text-[--font-global] text-center md:text-left  z-2 ">
           {t("title-text")}
           {/*Explanation*/}
           <span title={t("Explanation478")} className="cursor-pointer text-center"></span>
@@ -27,12 +27,12 @@ const WelcomePage = () => {
             id='infoButton' 
             title={t("Explanation478")}
             onClick={toggleInfo}
-            className="hover:underline flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[var(--color-button)] bg-[var(--color-button-text)] rounded-full shadow-md hover:opacity-80 text-sm sm:text-base max-w-[5.5rem] items-center justify-center"
+            className="hover:underline flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[#ffffff] bg-[var(--gradient-1-1)] rounded-full shadow-md hover:opacity-80 text-sm sm:text-base max-w-[5.5rem] items-center justify-center"
           >
             <p className="text-[--font-global]  text-[15px] font-bold">i</p>
           </button>
           <p id='infoText' 
-            className={`absolute flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[--font-global] text-[15px] text-[--color-text] bg-[var(--color-button-text)] rounded-md lg:mr-120 ${
+            className={`absolute flex px-3.5 sm:px-3.5 py-1 sm:py-1 text-[--font-global] text-[15px] text-[#ffffff] bg-[var(--gradient-1-1)] rounded-md lg:mr-120 ${
               isInfoVisible ? 'visible' : 'hidden'}`}
           >{t("Explanation478")}</p>
         </h2>
